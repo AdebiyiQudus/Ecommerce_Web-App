57e85896b463172acb3a3d393091abde8cf79238
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2024-04-09 21:29:57 +0100

    Responsive home page

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+'use strict'
 // Script for navigation bar
+const hamburger = document.getElementById('bar')
+const nav = document.getElementById('navbar');
+const closeHamburger = document.getElementById('close');
+
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    nav.classList.add('active')
+  });
+};
+
+if (closeHamburger) {
+  closeHamburger.addEventListener('click', () => {
+    nav.classList.remove('active')
+  });
+};
